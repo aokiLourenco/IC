80c45b6cf0d5b1740d32eb7cfc161c02a942d037
Aoki <aaokilourenco@ua.pt>
2024-11-04 14:16:56 +0000

    Split into P1 and P2

---
+++ Project1/build/CMakeFiles/P3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P3.dir/src/P3.cpp.o"
+  "CMakeFiles/P3.dir/src/P3.cpp.o.d"
+  "P3"
+  "P3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
